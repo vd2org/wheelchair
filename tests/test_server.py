6a2b89c0e9e20ba675adf72d01986d25b50c68f7
vd <vd@>
2020-05-30 02:03:49 +0300

    Adopted tests.

--- tests/test_server.py
+++ tests/test_server.py
@@ -12,3 +12,54 @@ async def test_server(admin_connection: Connection):
     res = await admin_connection.server()
 
     assert res['couchdb'] == 'Welcome'
+
+
+@pytest.mark.asyncio
+async def test_active_tasks(admin_connection: Connection):
+    res = await admin_connection.server.active_tasks()
+
+    assert isinstance(res, list)
+
+
+@pytest.mark.asyncio
+async def test_all_dbs(admin_connection: Connection):
+    res = await admin_connection.server.all_dbs()
+
+    assert isinstance(res, list)
+    assert isinstance(res[0], str)
+
+
+@pytest.mark.asyncio
+async def test_dbs_info(admin_connection: Connection):
+    res = await admin_connection.server.dbs_info(['_users'])
+
+    assert isinstance(res, list)
+    assert res[0]['key'] == '_users'
+
+
+@pytest.mark.asyncio
+async def test_membership(admin_connection: Connection):
+    res = await admin_connection.server.membership()
+
+    assert isinstance(res['all_nodes'], list)
+    assert isinstance(res['cluster_nodes'], list)
+
+
+@pytest.mark.asyncio
+async def test_up(admin_connection: Connection):
+    res = await admin_connection.server.up()
+
+    assert res['status'] == "ok"
+
+
+@pytest.mark.asyncio
+async def test_uuids(admin_connection: Connection):
+    res = await admin_connection.server.uuids()
+
+    assert isinstance(res, list)
+    assert len(res) == 1
+
+    res = await admin_connection.server.uuids(5)
+
+    assert isinstance(res, list)
+    assert len(res) == 5
--- tests/test_session.py
+++ tests/test_session.py
@@ -8,6 +8,15 @@ from wheelchair import Connection
 
 
 @pytest.mark.asyncio
+async def test_session(admin_connection: Connection):
+    await admin_connection.session.authenticate()
+    res = await admin_connection.session()
+
+    assert res.userCtx['name'] == 'admin'
+    assert len(res.userCtx['roles']) > 0
+
+
+@pytest.mark.asyncio
 async def test_authenticate(admin_connection: Connection):
     res = await admin_connection.session.authenticate()
 
@@ -16,19 +25,10 @@ async def test_authenticate(admin_connection: Connection):
 
 
 @pytest.mark.asyncio
-async def test_get(admin_connection: Connection):
-    await admin_connection.session.authenticate()
-    res = await admin_connection.session.get()
-
-    assert res['userCtx']['name'] == 'admin'
-    assert len(res['userCtx']['roles']) > 0
-
-
-@pytest.mark.asyncio
 async def test_delete(admin_connection: Connection):
     await admin_connection.session.authenticate()
     await admin_connection.session.delete()
-    res = await admin_connection.session.get()
+    res = await admin_connection.session()
 
-    assert res['userCtx']['name'] is None
-    assert res['userCtx']['roles'] == []
+    assert res.userCtx['name'] is None
+    assert res.userCtx['roles'] == []
