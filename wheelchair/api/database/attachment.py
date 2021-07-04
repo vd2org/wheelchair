f0748452169822eb6af89cc3a1ccd43b5cf63f2c
ivan <vd2org@users.noreply.github.com>
2021-07-04 14:22:10 +0300

    Change year.

--- wheelchair/api/database/attachment.py
+++ wheelchair/api/database/attachment.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2020 by Vd.
+# Copyright (C) 2019-2021 by Vd.
 # This file is part of Wheelchair, the async CouchDB connector.
 # Wheelchair is released under the MIT License (see LICENSE).
 from io import IOBase
