9182a2f2c92c950c21a157b2f8b68b27bfc8f367
ivan <vd2org@users.noreply.github.com>
2021-07-04 14:35:29 +0300

    Added `scheduler.jobs` and `scheduler.job` methods.

--- wheelchair/api/server/scheduler.py
+++ wheelchair/api/server/scheduler.py
@@ -19,3 +19,29 @@ class Scheduler(SimpleScope):
         params = dict(limit=limit, skip=skip)
 
         return await self._connection.query('GET', ['_scheduler', 'jobs'], params=params)
+
+    async def docs(self, replicator_db: Optional[str] = None, limit: Optional[int] = None,
+                   skip: Optional[int] = None) -> dict:
+        """\
+        List of persistent replication jobs.
+
+        https://docs.couchdb.org/en/stable/api/server/common.html#get--_scheduler-docs
+        https://docs.couchdb.org/en/stable/api/server/common.html#get--_scheduler-docs-replicator_db
+        """
+
+        params = dict(limit=limit, skip=skip)
+
+        path = ['_scheduler', 'docs']
+        if replicator_db:
+            path.append(replicator_db)
+
+        return await self._connection.query('GET', path, params=params)
+
+    async def doc(self, replicator_db: str, doc_id: str) -> dict:
+        """\
+        Get a persistent replication job.
+
+        https://docs.couchdb.org/en/stable/api/server/common.html#get--_scheduler-jobs
+        """
+
+        return await self._connection.query('GET', ['_scheduler', 'docs', replicator_db, doc_id])
